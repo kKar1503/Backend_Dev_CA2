20bce3900a717786a33210961bfad93357f32b71
kKar1503 <isonice4572@gmail.com>
2022-02-06 07:15:11 +0800

    Finished heart prompt for category

--- public/js/index.html.js
+++ public/js/index.html.js
@@ -9,7 +9,11 @@ axios
  * @param {Array} categories
  */
 function printCategories(categories) {
-	categories.forEach((category) => {
+	categories.forEach(async (category) => {
+		let categoryId = category.categoryid;
+		let categories = await axios.get('http://localhost:3000/interest/3');
+		let hasCategory = categories.data.some((category) => category.fk_category_id == categoryId);
+		console.log(hasCategory);
 		const categoryHtml = `<!-- mt product2 start here -->
         <div class="mt-product2 large bg-grey">
             <!-- box start here -->
@@ -17,7 +21,9 @@ function printCategories(categories) {
                 <img alt="image description" src="http://placehold.it/275x290" />
                 <ul class="links">
                     <li>
-                        <a href="#" class="category" id="category-${category.categoryid}"><i class="icon-heart"></i></a>
+                        <a class="category" id="category-${categoryId}"><i class="fa ${hasCategory ? 'fa-heart' : 'fa-heart-o'}" id="hi-${
+			category.categoryid
+		}"></i></a>
                     </li>
                     <li>
                         <a href="#"><i class="fa fa-eye"></i></a>
@@ -37,9 +43,15 @@ function printCategories(categories) {
 }
 
 $j(document).on('click', '.category', function () {
-	localStorage.setItem('productCategory', parseInt($j(this).attr('id').substring(9)));
-	paramObj = new Object();
-	paramObj.category = parseInt($j(this).attr('id').substring(9));
+	const categoryId = parseInt($j(this).attr('id').substring(9));
+	// axios
+	// 	.post(`http://localhost:3000/interest/3/${categoryId}`)
+	// 	.then(() => console.log('done'))
+	// 	.catch((err) => console.error(err));
+	$j(this).find('.fa-heart-o').toggleClass('fa-heart-o fa-heart');
+
+	// paramObj = new Object();
+	// paramObj.category = parseInt($j(this).attr('id').substring(9));
 	// paramObj.brand =
-	window.location.href = `/product?${$.param(paramObj)}`;
+	// window.location.href = `/product?${$.param(paramObj)}`;
 });

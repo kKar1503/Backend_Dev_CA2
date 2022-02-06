1e23f7afa84e96952b53e918858e502baec74536
Kehan <likehan217@gmail.com>
2022-02-06 12:52:11 +0800

    get back the rating bar

--- public/js/product-detail.html.js
+++ public/js/product-detail.html.js
@@ -104,7 +104,7 @@ function showAvgRating() {
 
 			$j('#avgRating').append(avgReview);
 			$j('#numReviews').append(
-				`<a href="#tab3" class="active">
+				`<a href="#tab2" class=''>
 					REVIEWS (${numReview})
 				</a>`
 			);
